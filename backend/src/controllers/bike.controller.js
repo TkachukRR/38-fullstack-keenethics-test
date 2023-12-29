df0b2f8cd7c8184dc87786d28a910bad2efb47cc
Roman Tkachuk <tkachukrr@gmail.com>
2023-12-29 23:51:11 +0100

    Created bike controller with structure

---
+++ backend/src/controllers/bike.controller.js
@@ -0,0 +1,24 @@
+exports.createBike = async (req, res) => {
+  try {
+  } catch (error) {}
+};
+
+exports.getAllBikes = async (req, res) => {
+  try {
+  } catch (error) {}
+};
+
+exports.removeBikeById = async (req, res) => {
+  try {
+  } catch (error) {}
+};
+
+exports.changeBikeStatus = async (req, res) => {
+  try {
+  } catch (error) {}
+};
+
+exports.getBikesStats = async (req, res) => {
+  try {
+  } catch (error) {}
+};
--- backend/src/routes/bike.routes.js
+++ backend/src/routes/bike.routes.js
@@ -1,12 +1,13 @@
 const { Router } = require('express');
+const bikeController = require('../controllers/bike.controller');
 const router = Router();
 
-router.post('/bikes/create', (req, res) => {});
-router.get('/bikes/all', (req, res) => {});
-router.delete('/bikes/delete/:id', (req, res) => {});
+router.post('/bikes/create', bikeController.createBike);
+router.get('/bikes/all', bikeController.getAllBikes);
+router.delete('/bikes/delete/:id', bikeController.removeBikeById);
 
-router.put('/bikes/:id/status', (req, res) => {});
+router.put('/bikes/:id/status', bikeController.changeBikeStatus);
 
-router.get('/bikes/stats', (req, res) => {});
+router.get('/bikes/stats', bikeController.getBikesStats);
 
 module.exports = router;
