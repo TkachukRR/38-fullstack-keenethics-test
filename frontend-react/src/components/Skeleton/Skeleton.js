41654389ba9fb52c670758b77f0ea18ed1f56696
Roman Tkachuk <tkachukrr@gmail.com>
2024-01-05 00:58:58 +0100

    Created Skeleton component

---
+++ frontend-react/src/components/Skeleton/Skeleton.js
@@ -0,0 +1,29 @@
+import classes from './Skeleton.module.css';
+import { useEffect, useRef, useState } from 'react';
+
+export default function Skeleton({ width }) {
+  const skeleton = useRef();
+  const [styles, setStyles] = useState({});
+
+  useEffect(() => {
+    const parentElement = skeleton?.current?.parentElement;
+    const parentStyles = getComputedStyle(parentElement);
+    const parentLineHeight = parseInt(parentStyles.lineHeight);
+    const parentFontSize = parseInt(parentStyles.fontSize);
+    const newStyles = {
+      height: parentFontSize + 'px',
+      marginTop: (parentLineHeight - parentFontSize) / 2 + 'px',
+      marginBottom: (parentLineHeight - parentFontSize) / 2 + 'px',
+      width: width || '100px',
+    };
+    setStyles(() => newStyles);
+  }, []);
+
+  return (
+    <span
+      ref={skeleton}
+      className={classes.skeleton}
+      style={{ ...styles }}
+    ></span>
+  );
+}
