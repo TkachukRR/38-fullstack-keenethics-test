0dea74017089d74946275756fcfda619b86e2c58
Roman Tkachuk <tkachukrr@gmail.com>
2024-01-04 01:10:35 +0100

    Implemented ProductList item border style changing by product status

--- frontend-react/src/components/ProductList/ProductList.js
+++ frontend-react/src/components/ProductList/ProductList.js
@@ -6,7 +6,14 @@ export default function ProductList({ bikes }) {
     <ul className={classes.bike__list}>
       {bikes.map((bike) => {
         return (
-          <li className={classes.bike__item} key={bike.ID}>
+          <li
+            className={classes.bike__item}
+            style={{
+              borderColor: `var(--color-product-card-border-${bike.status})`,
+              opacity: bike.status === 'unavailable' ? '.5' : '',
+            }}
+            key={bike.ID}
+          >
             <ProductCard product={bike} />
           </li>
         );
